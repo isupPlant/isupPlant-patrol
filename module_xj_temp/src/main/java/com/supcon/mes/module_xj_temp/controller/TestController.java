362960c22f8f966177fe424e1c6afae377374d87
wangshizhan <wangshizhan@supcon.com>
2020-06-04 17:02:13 +0800

    上传巡检业务模块代码

---
+++ module_xj_temp/src/main/java/com/supcon/mes/module_xj_temp/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.supcon.mes.module_xj_temp.controller;
+
+import com.supcon.common.view.base.controller.BaseController;
+
+/**
+ * Created by wangshizhan on 2020/6/4
+ * Email:wangshizhan@supcom.com
+ */
+public class TestController extends BaseController {
+}
