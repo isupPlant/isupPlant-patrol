61fcf32d3afa12b74ddcf65fd3c9c90926805b80
wangnana <wangnana2@supcon.com>
2021-04-13 18:52:12 +0800

    [CHG]:1.错误日志打开

--- module_defectmanage/src/main/java/com/supcon/mes/module_defectmanage/presenter/AddDefectPresenter.java
+++ module_defectmanage/src/main/java/com/supcon/mes/module_defectmanage/presenter/AddDefectPresenter.java
@@ -1,7 +1,9 @@
 package com.supcon.mes.module_defectmanage.presenter;
 
+import com.supcon.mes.mbap.utils.GsonUtil;
 import com.supcon.mes.middleware.model.bean.BAP5CommonEntity;
 import com.supcon.mes.middleware.model.bean.FunctionEx;
+import com.supcon.mes.middleware.util.FileUtil;
 import com.supcon.mes.module_defectmanage.model.bean.DefectModelEntity;
 import com.supcon.mes.module_defectmanage.model.contract.AddDefectContract;
 import com.supcon.mes.module_defectmanage.model.network.DefectManagerHttpClient;
@@ -21,6 +23,9 @@ public class AddDefectPresenter extends AddDefectContract.Presenter {
                 .onErrorReturn(new FunctionEx<Throwable, Object>() {
                     @Override
                     public BAP5CommonEntity apply(Throwable throwable)  {
+                        //写入到本地接口日志文件中；
+                        String fileName = FileUtil.getFilePathAndName(FileUtil.NETWORK_REQUEST_LOG);
+                        FileUtil.write2FileAppend(fileName, (Throwable) throwable);
                         return super.apply(throwable);
                     }
                 }).subscribe(new Consumer<BAP5CommonEntity<Object>>() {
@@ -29,6 +34,10 @@ public class AddDefectPresenter extends AddDefectContract.Presenter {
                         if (incidentTmpEntityCommonEntity.success) {
                             getView().defectEntrySuccess(incidentTmpEntityCommonEntity);
                         } else {
+                            //写入到本地接口日志文件中；
+                            String fileName = FileUtil.getFilePathAndName(FileUtil.NETWORK_REQUEST_LOG);
+                            FileUtil.write2FileAppend(fileName, GsonUtil.gsonString(info));
+
                             getView().defectEntryFailed(incidentTmpEntityCommonEntity.msg);
                         }
                     }
@@ -41,6 +50,10 @@ public class AddDefectPresenter extends AddDefectContract.Presenter {
                 .onErrorReturn(new FunctionEx<Throwable, Object>() {
                     @Override
                     public BAP5CommonEntity apply(Throwable throwable)  {
+                        //写入到本地接口日志文件中；
+                        String fileName = FileUtil.getFilePathAndName(FileUtil.NETWORK_REQUEST_LOG);
+                        FileUtil.write2FileAppend(fileName, (Throwable) throwable);
+
                         return super.apply(throwable);
                     }
                 }).subscribe(new Consumer<BAP5CommonEntity<Object>>() {
@@ -49,6 +62,10 @@ public class AddDefectPresenter extends AddDefectContract.Presenter {
                         if (incidentTmpEntityCommonEntity.success) {
                             getView().defectEntryBatchSuccess(incidentTmpEntityCommonEntity);
                         } else {
+                            //写入到本地接口日志文件中；
+                            String fileName = FileUtil.getFilePathAndName(FileUtil.NETWORK_REQUEST_LOG);
+                            FileUtil.write2FileAppend(fileName, GsonUtil.gsonString(infoList));
+
                             getView().defectEntryBatchFailed(incidentTmpEntityCommonEntity.msg);
                         }
                     }
